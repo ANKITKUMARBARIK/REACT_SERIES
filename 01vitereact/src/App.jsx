e96056e88879bb5aaf358043981d8ab931f7d6de
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2024-12-27 17:31:42 +0530

    first project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Syncodx from './Syncodx'
+
+function App() {
+  const username = 'syncodx code'
+  return (
+    <>
+      <h1>synCodx</h1>
+      <h2>Username : {username}</h2>
+      <Syncodx />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Syncodx.jsx
@@ -0,0 +1,9 @@
+function Syncodx() {
+    return (
+        <>
+            <h1>Syncodx processing...</h1>
+        </>
+    )
+}
+
+export default Syncodx
