99079e747ef6bbe2bd7a11d68020d22a8a2a8928
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-01-12 20:28:33 +0530

    thirty-third project

--- 33vitereact/src/App.jsx
+++ 33vitereact/src/App.jsx
@@ -2,6 +2,10 @@ import { useCallback, useState } from 'react'
 import './App.css'
 import ChildA from './ChildA'
 
+// useCallback ek React hook hai jo ek function ko memoize karta hai, yaani usse repeat computation se bachata hai. Iska use tab kiya jata hai jab aap kisi function ko child component ko prop ke roop mein pass kar rahe ho, aur aap nahi chahte ki har render ke saath wo function dobara se banaye.
+
+// Jab aap useCallback ka use karte ho, to React function ko wahi reference dene ki koshish karta hai, jab tak ki dependencies nahi badalti hain. Isse unnecessary re-renders aur computations bachti hain.
+
 function App() {
   const [countOne, setCountOne] = useState(0)
   const [countTwo, setCountTwo] = useState([])
